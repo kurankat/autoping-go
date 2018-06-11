55e8a8c4fcb7cc6b0856ffa4db38f9cee9c262ce
Miguel <mfs@mail.xeryllium.com>
2018-06-11 22:36:58 +1000

    Trace log & move evaluation

--- autoping.go
+++ autoping.go
@@ -6,6 +6,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
 	"os"
@@ -32,7 +33,8 @@ type connTracker struct {
 
 // Set up flags, loggers and global variables
 var importFlag = flag.String("i", "", "IP address or hostname to be pinged")
-var pLog, eLog, oLog *log.Logger
+var traceFlag = flag.Bool("t", false, "turn on trace to file")
+var pLog, eLog, oLog, tLog *log.Logger
 var ipAddr string // User supplied IP address to ping to
 
 var connInfo = connTracker{isOutage: false}
@@ -67,6 +69,11 @@ func main() {
 	pLog = log.New(logFile, "PING - ", log.LstdFlags)
 	eLog = log.New(logFile, "ERROR - ", log.LstdFlags)
 	oLog = log.New(logFile, "OUTAGE - ", log.LstdFlags)
+	tLog = log.New(ioutil.Discard, "TRACE - ", log.LstdFlags)
+
+	if *traceFlag {
+		tLog.SetOutput(logFile)
+	}
 
 	// Set up channel and goroutine to handle interrupts
 	c := make(chan os.Signal, 1)
@@ -78,9 +85,11 @@ func main() {
 			os.Exit(1)
 		}
 	}()
+	tLog.Printf("Setting up channel to handle interrupts")
 
 	// Launch separate goroutine to carry out ping every minute
 	for {
+		tLog.Printf("Running ping now")
 		go runPing()
 		time.Sleep(1 * time.Minute)
 	}
@@ -90,6 +99,7 @@ func main() {
 func runPing() {
 	// Set up pinger and handle errors
 	t := time.Now() // Keep track of the time the ping was sent
+	tLog.Printf("Setting Ping time to %v", t)
 	pinger, err := ping.NewPinger(ipAddr)
 	if err != nil {
 		switch err.(type) {
@@ -98,6 +108,7 @@ func runPing() {
 				t.Sub(connInfo.lastSuccessfulPing) > 2*time.Minute {
 				connInfo.isOutage = true
 				connInfo.outageDuration = time.Now().Sub(connInfo.lastSuccessfulPing)
+				tLog.Printf("DNS error")
 				oLog.Printf("Lost contact. Outage duration %v",
 					connInfo.outageDuration)
 			}
@@ -107,45 +118,49 @@ func runPing() {
 	} else {
 		// Pinger settings.
 		pinger.Count = 1
+		tLog.Printf("Setting pinger count to %v", pinger.Count)
 		pinger.Timeout = 30 * time.Second
+		tLog.Printf("Setting pinger timeout to %v", pinger.Timeout)
 		pinger.SetPrivileged(true) // Needed to process TCP pings
+		tLog.Printf("Setting pinger to privileged")
 
 		// What to do when ping comes in: log results
 		pinger.OnRecv = func(pkt *ping.Packet) {
 			pLog.Printf("%d bytes from %s: icmp_seq=%d time=%v", pkt.Nbytes, pkt.IPAddr,
 				pkt.Seq, pkt.Rtt)
-			evaluateLatency(t, pkt.Rtt)
 		}
-		pinger.Run() // Send the ping
-
-		// If no packets come back after timeout, start logging outage after 2 min
-		// since last successful ping (2 missed pings in a row)
-		if pinger.Statistics().PacketsRecv == 0 {
-			// The following conditions have to be met: the ping year of the last
-			// successful ping has to be this year (at the start of the run lsPing is
-			// set to 0) AND the time difference between the last successful ping and
-			// this one has to be more than 2 minutes
-			oLog.Printf("Timeout - Missed pong")
-			if connInfo.lastSuccessfulPing.Year() == t.Year() &&
-				t.Sub(connInfo.lastSuccessfulPing) > 2*time.Minute {
-				connInfo.isOutage = true
-				connInfo.outageDuration = time.Now().Sub(connInfo.lastSuccessfulPing)
-				oLog.Printf("Lost contact. Outage duration %v minutes",
-					connInfo.outageDuration)
+		pinger.OnFinish = func(s *ping.Statistics) {
+			// If no packets come back after timeout, start logging outage after 2 min
+			// since last successful ping (2 missed pings in a row)
+			if s.PacketsRecv == 0 {
+				tLog.Printf("Pinger timed out")
+				// The following conditions have to be met: the ping year of the last
+				// successful ping has to be this year (at the start of the run lsPing is
+				// set to 0) AND the time difference between the last successful ping and
+				// this one has to be more than 2 minutes
+				oLog.Printf("Timeout - Missed pong")
+				if connInfo.lastSuccessfulPing.Year() == t.Year() &&
+					t.Sub(connInfo.lastSuccessfulPing) > 2*time.Minute {
+					connInfo.isOutage = true
+					connInfo.outageDuration = time.Now().Sub(connInfo.lastSuccessfulPing)
+					oLog.Printf("Lost contact. Outage duration %v minutes",
+						connInfo.outageDuration)
+				}
+			} else if s.PacketsRecv > 0 {
+				// If we get a packet back, reset last successful ping time to the time this
+				// ping was fired, and reset outage
+				if connInfo.isOutage {
+					oLog.Printf("Connection restored. Total outage duration %v",
+						connInfo.outageDuration)
+				}
+				connInfo.lastSuccessfulPing = t
+				connInfo.isOutage = false
+				tLog.Printf("Packet recieved. Sending to evaluateLatency()")
+				evaluateLatency(t, s.MinRtt)
 			}
-		}
-
-		// If we get a packet back, reset last successful ping time to the time this
-		// ping was fired, and reset outage
-		if pinger.Statistics().PacketsRecv > 0 {
-			if connInfo.isOutage {
-				oLog.Printf("Connection restored. Total outage duration %v",
-					connInfo.outageDuration)
-			}
-			connInfo.lastSuccessfulPing = t
-			connInfo.isOutage = false
 		}
 	}
+	pinger.Run() // Send the ping
 }
 
 // Evaluate latency of supplied ping. If ping has a long latency, add it to the
@@ -153,39 +168,60 @@ func runPing() {
 // normal. If so, finalise spl and log total duration of dodgy latency pings.
 // If previous ping was dodgy, ignore single normal ping and keep logging
 func evaluateLatency(t time.Time, rtt time.Duration) {
+	tLog.Printf("Evaluating Pong sent at %v with RTT of %v", t, rtt)
 	meanLat = time.Duration(latSlice.mean()) * time.Nanosecond
+	tLog.Printf("meanLat is currently %v", meanLat)
 	cutoff := meanLat * 3
 	prd := false // The previous ping is never dodgy by default
 
 	// Set up the provious dodgy ping to be that of the last item in spl
 	if len(spl) > 0 {
 		prd = spl[len(spl)-1].crDod // Set prd to the RTT of the previous dodgy ping
+		tLog.Printf("Setting prd to %v", spl[len(spl)-1].crDod)
+	} else {
+		tLog.Printf("spl length is 0")
 	}
 
 	// If the ping RTT is more than the cutoff, treat as a dodgy ping and append
 	// to spl
 	if rtt > cutoff && cutoff > 0 {
+		tLog.Printf("Dodgy latency of %v", rtt)
 		dPing := dLatPing{crDod: true, prDod: prd, latency: rtt, pTime: t}
+		tLog.Printf("Creating dPing of %v", dPing)
 		spl = append(spl, dPing)
+		tLog.Printf("Total spl is %v", spl)
 	} else {
 		// Because this is a 'normal' ping RTT, append it to queue to keep a running
 		// average
 		latSlice.add(float64(rtt.Nanoseconds()))
+		tLog.Printf("RTT of %v is normal. Adding it to latency slice to keep average",
+			float64(rtt.Nanoseconds()))
 
 		// If the latency is OK, check that of previous. If that one is dodgy,
 		// keep logging until two consecutive normal pings
 		if prd {
+			tLog.Printf("Previous ping was dodgy and had an RTT of %v",
+				spl[len(spl)-1].latency)
 			dPing := dLatPing{crDod: false, prDod: prd, latency: rtt, pTime: t}
+			tLog.Printf("Because this Ping had a normal RTT, dPing is set to %v", dPing)
 			spl = append(spl, dPing)
+			tLog.Printf("Appending to spl. Current spl = %v", spl)
 		} else {
-
 			// If two decent latency pings in a row, then log total and reset spl
 			if len(spl) > 2 {
+				tLog.Printf("Previous Ping and this Ping both have normal latencies: %v and %v",
+					spl[len(spl)-1].latency, rtt)
+				tLog.Printf("Calculating bad run and resetting spl")
 				startTime := spl[0].pTime
+				tLog.Printf("Start of dodgy latency run: %v", startTime)
 				endTime := spl[len(spl)-1].pTime
+				tLog.Printf("End of dodgy latency run: %v", endTime)
 				oLog.Printf("Period of flakey latency finished. Duration = %v",
 					endTime.Sub(startTime))
 				spl = nil
+				tLog.Printf("Resetting spl: %v", spl)
+			} else {
+				tLog.Printf("Length of spl is less than 2: %v", len(spl))
 			}
 		}
 	}
